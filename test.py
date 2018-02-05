d954cc0b6821dbbe01be6e99f3eeaafe69f2ab50
Jay Jackson <jaynickjackson@gmail.com>
2018-02-05 15:38:06 -0500

    added sqlite3

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('data2.db')
+connection = sqlite3.connect('data.db')
 
 cursor = connection.cursor() # responsible for executing queries
 
-# create_table = "CREATE TABLE users (id int, username text, password text)" # schema
-# cursor.execute(create_table)
+create_table = "CREATE TABLE users (id int, username text, password text)" # schema
+cursor.execute(create_table)
 
 user = (1, 'jay', 'pass', )
 insert_query = "INSERT INTO users VALUES (?, ?, ?)"
 cursor.execute(insert_query, user)
 
-connection.commit()
+users = [
+  (2, 'rolf', 'asdf'),
+  (3, 'anne', 'adsf')
+]
+cursor.executemany(insert_query, users)
 
+connection.commit()
 connection.close()
